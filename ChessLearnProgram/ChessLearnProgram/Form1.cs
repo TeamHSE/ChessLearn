3422bdfbcface9833f9487b95f42cd42210f328d
AleksanderNekr <aleksandr_nekrasov@outlook.com>
2022-04-04 23:06:34 +0500

    Created solution.

---
+++ ChessLearnProgram/ChessLearnProgram/Form1.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace ChessLearnProgram
+{
+    internal sealed partial class Form1 : Form
+    {
+        public Form1() => this.InitializeComponent();
+    }
+}
