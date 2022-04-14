21267b4701be730cd3048f3955fa755bed106c86
Захар Молчанов <zvmolchanov@edu.hse.ru>
2022-04-14 20:30:54 +0500

    Added chess board.

--- ChessLearnProgram/ChessLearnProgram/Form1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Forms;
-
-namespace ChessLearnProgram
-{
-    internal sealed partial class Form1 : Form
-    {
-        public Form1() => this.InitializeComponent();
-    }
-}
