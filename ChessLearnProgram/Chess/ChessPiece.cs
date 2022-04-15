5be390c8b3a3a61f6189b0d42b314d054b74a906
AleksanderNekr <aleksandr_nekrasov@outlook.com>
2022-04-15 22:43:53 +0500

    Added doc comments.

--- ChessLearnProgram/Chess/ChessPiece.cs
+++ ChessLearnProgram/Chess/ChessPiece.cs
@@ -4,19 +4,36 @@ namespace Chess
 {
     public abstract class ChessPiece
     {
+        /// <summary>
+        /// Текущая координата фигуры.
+        /// </summary>
         public abstract Coordinate CurrentCoordinate { get; set; }
 
+        /// <summary>
+        /// Переход на другую клетку.
+        /// </summary>
+        /// <param name="coordinate">Координата другой клетки.</param>
         public void MoveTo(Coordinate coordinate) => CurrentCoordinate = coordinate;
 
+        /// <summary>
+        /// Список корректных возможных ходов.
+        /// </summary>
         public abstract List<Coordinate> ValidMoves { get; set; }
 
+        /// <summary>
+        /// Статус фигуры. True – не срублена, False – срублена.
+        /// </summary>
         public abstract bool IsPlayable { get; set; }
 
-
+        /// <inheritdoc />
+        /// <summary>
+        /// Координата шахматной фигуры.
+        /// </summary>
         public abstract class Coordinate : ICoordinate
         {
             public int Column { get; set; }
-            public int Row    { get; set; }
+
+            public int Row { get; set; }
 
             internal Coordinate(int column, int row)
             {
--- ChessLearnProgram/Chess/ICoordinate.cs
+++ ChessLearnProgram/Chess/ICoordinate.cs
@@ -1,9 +1,18 @@
 ﻿namespace Chess
 {
+    /// <summary>
+    /// Интерфейс, описывающий координату.
+    /// </summary>
     internal interface ICoordinate
     {
+        /// <summary>
+        /// Координата столбца.
+        /// </summary>
         public int Column { get; set; }
 
+        /// <summary>
+        /// Координата строки.
+        /// </summary>
         public int Row { get; set; }
     }
 }
