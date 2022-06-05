d7051139d4c83b23b2457b08019db5f91d1cd7df
AleksanderNekr <aleksandr_nekrasov@outlook.com>
2022-06-05 17:40:10 +0500

    Separated valid move class.

--- ChessLearnProgram/Chess/Pieces/ChessPiece.cs
+++ ChessLearnProgram/Chess/Pieces/ChessPiece.cs
@@ -148,31 +148,4 @@ namespace Chess.Pieces
             return (col >= 0) && (col < 8) && (row >= 0) && (row < 8);
         }
     }
-
-    public sealed class ValidMove : ChessPiece
-    {
-        public ValidMove(Coordinate coordinate, string color) : base(coordinate, color)
-        {
-            this.AllowDrop = true;
-            this.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            this.AutoSize = true;
-            this.BackColor = System.Drawing.Color.Chartreuse;
-            this.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.FlatAppearance.BorderSize = 0;
-            this.FlatStyle = FlatStyle.Flat;
-            this.UseVisualStyleBackColor = true;
-            ChessBoard.ChessBoardMatrix[coordinate.Column, coordinate.Row] = this;
-            this.Color = "White";
-        }
-
-        public Coordinate Coordinate
-        {
-            get { return this.CurrentCoordinate; }
-        }
-
-        public override List<Coordinate> GetValidMoves()
-        {
-            return null;
-        }
-    }
 }
